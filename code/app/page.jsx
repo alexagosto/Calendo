4749d6967ebdc8b447969ef5d1c3c095a0286764
jorgeortiz41 <jorge.ortiz41@upr.edu>
2024-04-13 05:29:33 -0400

    removed media light/dark mode and fixed window referenceError

--- code/app/page.jsx
+++ code/app/page.jsx
@@ -1,18 +1,20 @@
-'use client'
+"use client";
 
-import { redirect } from 'next/navigation'
-import Calendar from './calendar/calendar';
+import { redirect } from "next/navigation";
+import Calendar from "./calendar/calendar";
 
 //TODO: Fix dark mode styles, for ease of use keep everything in light mode
 export default function Home() {
-  const email = sessionStorage.getItem('email');
-  const password = sessionStorage.getItem('password');
-  const name = localStorage.getItem(email + 'name');
+  if (typeof window !== "undefined") {
+    // do your stuff with sessionStorage
+    const email = sessionStorage.getItem("email");
+    const password = sessionStorage.getItem("password");
+    const name = localStorage.getItem(email + "name");
 
-  if (localStorage.getItem(email)  == null) {
-    redirect('/auth');
+    if (localStorage.getItem(email) == null) {
+      redirect("/auth");
+    }
   }
-  
 
   return (
     <div className="flex flex-col items-center h-screen text-sm">
